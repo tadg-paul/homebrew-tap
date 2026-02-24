c0567e0bb3bdff5582b952502e0f1a2f805a4ac9
tigger <tigger04@gmail.com>
2026-02-24 16:56:42 +0000

    storyboard-gen 0.50.0

--- Formula/storyboard-gen.rb
+++ Formula/storyboard-gen.rb
@@ -1,8 +1,8 @@
 class StoryboardGen < Formula
   desc "Generate video stills and clips from a YAML storyboard using AI providers"
   homepage "https://github.com/tigger04/storyboard-gen"
-  url "https://github.com/tigger04/storyboard-gen/archive/refs/tags/v0.49.0.tar.gz"
-  sha256 "fb7a910eaa72b1e2f72cbb8001ff24c69c7916ba5b1d09ce810019afa002746c"
+  url "https://github.com/tigger04/storyboard-gen/archive/refs/tags/v0.50.0.tar.gz"
+  sha256 "672813c7501e655879a0391116837574684fdc65c7ab62c5b43c89aacf145a71"
   license "MIT"
 
   depends_on "python@3.12"
