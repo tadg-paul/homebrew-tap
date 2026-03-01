2abc26151ecf43db2870a2d370701ffbc28ed5cd
tigger <tigger04@gmail.com>
2026-03-01 00:23:17 +0000

    make-audiobook: auto-install kokoro-tts with Python 3.12

--- Formula/make-audiobook.rb
+++ Formula/make-audiobook.rb
@@ -12,12 +12,13 @@ class MakeAudiobook < Formula
   depends_on "bash" => "5.0"
   # Note: calibre is needed for .mobi support but must be installed separately as a cask:
   # brew install --cask calibre
-  depends_on "espeak" => :optional      # for Kokoro TTS engine
+  depends_on "espeak"                    # required by Kokoro TTS engine
   depends_on "ffmpeg"
   depends_on "pandoc"
   depends_on "fzf"
   depends_on "fd"
   depends_on "pipx"
+  depends_on "python@3.12"              # kokoro-tts requires Python <3.13
 
   def install
     bin.install "make-audiobook"
@@ -26,7 +27,16 @@ class MakeAudiobook < Formula
   end
 
   def post_install
-    ohai "To install piper-tts, run: pipx install piper-tts"
+    ohai "Installing piper-tts via pipx..."
+    system "pipx", "install", "piper-tts"
+    # kokoro-tts requires Python <3.13; find highest compatible version
+    py312 = Formula["python@3.12"].opt_bin/"python3.12"
+    if py312.exist?
+      ohai "Installing kokoro-tts via pipx (Python 3.12)..."
+      system "pipx", "install", "kokoro-tts", "--python", py312
+    else
+      ohai "kokoro-tts requires Python <3.13. Install python@3.12, then: pipx install kokoro-tts --python python3.12"
+    end
     ohai "To install default voices, run: piper-voices-setup"
   end
 
