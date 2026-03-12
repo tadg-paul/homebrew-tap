dc1a9bd7c3dbb58bafb10c38242f44c6c56a9997
tigger <tigger04@gmail.com>
2026-03-12 18:29:44 +0000

    storyboard-gen-gui 0.65.0

--- Casks/storyboard-gen-gui.rb
+++ Casks/storyboard-gen-gui.rb
@@ -2,8 +2,8 @@
 # ABOUTME: Installs the PySide6 GUI as a standalone macOS .app bundle.
 
 cask "storyboard-gen-gui" do
-  version "0.61.0"
-  sha256 "75a9edb46840c69cba712794034062a7b8ead813d3db1fe58e8a774cafc7e3d3"
+  version "0.65.0"
+  sha256 "26a2ff28e531417a09bcf2190c9247ca4f7f43f2a86b790584c35a622cc667a3"
 
   url "https://github.com/tigger04/storyboard-gen/releases/download/v#{version}/storyboard-gen-gui-#{version}.dmg"
   name "Storyboard Gen"
