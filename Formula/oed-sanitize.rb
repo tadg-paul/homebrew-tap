1aa297b00c5baafaf97b23c9219edfdc7221677c
tigger <tigger04@gmail.com>
2026-06-12 21:32:18 +0100

    Update oed-sanitize formula to v0.8.0

--- Formula/oed-sanitize.rb
+++ Formula/oed-sanitize.rb
@@ -1,14 +1,14 @@
 class OedSanitize < Formula
   desc "Fast CLI tool for converting English text to Oxford (OED) spelling"
   homepage "https://github.com/tigger04/oed-sanitize"
-  url "https://github.com/tigger04/oed-sanitize/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "b58f9e5877117a1e72f9e73d693ea8352fbcc9b939e40146efd059b51889a967"
+  url "https://github.com/tigger04/oed-sanitize/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "53e339ac5d3e272a5a6dd59b0db65b453be89cafeda6cc4971df8e2948220f42"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-X main.version=0.6.0", "-o", bin/"sanitize", "./cmd/sanitize/"
+    system "go", "build", "-ldflags", "-X main.version=0.8.0", "-o", bin/"sanitize", "./cmd/sanitize/"
   end
 
   test do
